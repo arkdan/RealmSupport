c414d5ffa7f2aa49c69eef108684ee3dc93e97aa
arkadzi.daniyelian <arkadzi.daniyelian@accenture.com>
2018-05-05 19:48:24 +0300

    initial

---
+++ RealmSupport/RealmSupport.h
@@ -0,0 +1,19 @@
+//
+//  RealmSupport.h
+//  RealmSupport
+//
+//  Created by ark dan on 05/05/2018.
+//  Copyright © 2018 arkdan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmSupport.
+FOUNDATION_EXPORT double RealmSupportVersionNumber;
+
+//! Project version string for RealmSupport.
+FOUNDATION_EXPORT const unsigned char RealmSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmSupport/PublicHeader.h>
+
+
